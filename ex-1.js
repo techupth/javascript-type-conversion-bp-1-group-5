231e9296b31edbadf13aab8f076d0f636fa2b1e8
Cynthy YD <cynthyyd@Cynthys-MacBook-Pro.local>
2023-10-04 03:10:12 +0700

    add: exercise answers

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,18 @@
+// Exercise #1: Debugging
+// Start code here
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
+<<<<<<< Updated upstream
+=======
+console.log(typeof accountBalance);
+console.log(typeof depositAmountFromUser);
+depositAmountFromUser = Number(depositAmountFromUser);
+console.log(typeof depositAmountFromUser);
+
+>>>>>>> Stashed changes
 accountBalance = accountBalance + depositAmountFromUser;
 
-let accountBalanceMessage = "Your account balance is " + accountBalance;
+let accountBalanceMessage = `Your account balance is ${accountBalance}`;
 
 console.log(accountBalanceMessage);
